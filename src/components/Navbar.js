c707e6947c41f751849c7cadaf54eb2daed325df
SoftPlus Systems <https://github.com/Joseph6655/nextjs-test3.git>
2024-04-03 13:22:50 +0300

    new changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <nav className="flex">
-      <div className="bg-gray-100 py-5 px-10 flex items-center w-1/2">
-        <div className="flex flex-col">
+    <nav className="flex relative overflow-hidden justify-center">
+      <div className="bg-gray-200 py-5 px-10 flex items-center w-full relative z-10">
+        <div className="container mx-auto">
           <div className="flex items-center">
-            <span className="text-black mr-1 font-bold text-3xl ml-10">Job</span>
-            <span className="text-green-400 font-bold mr-5 text-3xl">NQW</span>
-          
-            <ul className="navList flex mt-2">
+            <div className="flex items-center">
+              <span className="text-black font-bold text-3xl ml-16">Job</span>
+              <span className="text-green-400 font-bold mr-5 text-3xl">NQW</span>
+            </div>
+            <div className="lg:hidden">
+              <button onClick={toggleMenu} className="focus:outline-none">
+                <svg className={`h-6 w-6 fill-current ${showMenu ? 'text-white' : 'text-green-400'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  {showMenu ? (
+                    <path d="M19 13H5v-2h14v2zm0-5H5V6h14v2zm0 10H5v-2h14v2z" />
+                  ) : (
+                    <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
+                  )}
+                </svg>
+              </button>
+            </div>
+            <ul className={`lg:flex hidden ${showMenu ? 'flex bg-gray-200 w-full mt-2 py-2 rounded' : 'hidden'}`}>
               <li className="navItem mx-4">
-                <Link href="/" className="navLink text-black p-2 font-bold">Home</Link>
+                <Link href="/" className="navLink text-black p-2 font-bold ml-10">Home</Link>
               </li>
               <li className="navItem mx-4">
                 <Link href="/about" className="navLink text-black p-2 font-bold">Job</Link>
@@ -25,29 +43,35 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <div className="ml-10 mt-16 mr-3">
-            <h3 className="font-bold text-4xl">Search, Find, & Apply</h3>
-            <p className="mt-4">lorem ipsum dolor sit amet, consectetur adipiscing elit, Sed quis lacus non orci euismod vestibulum vitae ut ex, Quisque ut arcu at lectus tristique auctor sit amet at turpis.</p>
-          </div>
-          <div className="bg-white flex items-center mt-10 mb-16" style={{ borderRadius: '15px' }}>
-            <button className="bg-gray-100 flex text-black p-2 m-2 pr-16" style={{ borderRadius: '10px' }}>
-            <img src="/searchicon.png" alt="Location Icon" className="mr-2 mt-1 h-4 w-4"  />
-            Job title or keyword
-            </button>
-            <button className=" flex bg-gray-100 text-black p-2 m-2 pr-16" style={{ borderRadius: '10px' }}>
-              <img src="/locationicon.jpg" alt="Location Icon" className="mr-2 mt-1 h-4 w-4"  />
-              Location
-            </button>
-            <button className="bg-green-400 text-white p-2 m-2 pl-10 pr-10" style={{ borderRadius: '10px' }}>Search</button>
-          </div>
         </div>
-        
       </div>
-      <div className="bg-green-400  flex items-start justify-center w-1/2" style={{borderBottomLeftRadius: '20px'}}>
-        <div className="text-white flex items-center mt-5">
-          <div className="mr-10">Sign In</div>
-          <button className="bg-white text-black p-2 font-bold" style={{borderRadius: '10px'}}>Create Account</button>
+      
+      <div className={`lg:block hidden bg-green-300 py-5 px-10 flex items-center w-full relative z-10 ${showMenu ? '' : 'hidden'}`}>
+        <div className="container mx-auto">
+          <div className="flex items-center justify-end">
+            <div className="mr-10 text-white">Sign In</div>
+            <button className="flex bg-white text-black p-2 font-bold" style={{borderRadius: '10px'}}>
+              <img src="/arrowicon.jpg" alt="Location Icon" className="ml-3 mr-2 mt-1 h-3 w-3"  />
+              Create Account
+            </button>
+          </div>
         </div>
+      </div>
+      <div className="lg:hidden bg-green-300 py-5 px-10 flex items-center w-full absolute inset-0 z-0 animate-bg">
+        <ul className={`flex flex-col ${showMenu ? 'text-white' : 'hidden'}`}>
+          <li className="py-2 px-4">
+            <Link href="/" className="navLink">Home</Link>
+          </li>
+          <li className="py-2 px-4">
+            <Link href="/about" className="navLink">Job</Link>
+          </li>
+          <li className="py-2 px-4">
+            <Link href="/about" className="navLink">AboutUs</Link>
+          </li>
+          <li className="py-2 px-4">
+            <Link href="/contact" className="navLink">Contact</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import Navbar from '../components/Navbar';
 import React from 'react';
+import Head from 'next/head';
 
 
 
 const Home = () => {
   return (
-    
+    <div>
+      <Head>
+        <title>Home page</title>
+      </Head>
       <Navbar />
-      
+     
+      </div>
   );
 };
 
