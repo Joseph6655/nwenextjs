739946bd6cf5947144da60201f9a7175b4484f6e
SoftPlus Systems <https://github.com/Joseph6655/nextjs-test3.git>
2024-04-05 10:29:48 +0300

    new changes

--- src/components/AdditionalComponent.js
+++ src/components/AdditionalComponent.js
@@ -2,31 +2,40 @@ import React from 'react';
 
 const AdditionalComponent = () => {
   return (
-    <div className="bg-gray-200 py-0 max-w-[1350px]">
-      <div className="container mx-auto flex max-w-[1350px]">
-        
-        <div className="bg-gray-200 w-3/4 p-10 ">
-        <div className="ml-10 mt-16 mr-3">
-            <h3 className="font-bold text-4xl ml-9">Search, Find, & Apply</h3>
-            <p className="mt-4 ml-9">lorem ipsum dolor sit amet, consectetur adipiscing elit, Sed quis lacus non orci euismod vestibulum vitae ut ex, Quisque ut arcu at lectus tristique auctor sit amet at turpis.</p>
+    <div className="bg-gray-200 py-0">
+      <div className="container mx-auto flex justify-center lg:justify-start">
+        <div className="flex flex-col lg:flex-row">
+          
+          <div className="bg-gray-200 w-full lg:w-3/4 mr-0">
+            <div className="p-5">
+              <div className="ml-10 mt-16 mr-3">
+                <h3 className="font-bold text-4xl text-center lg:text-left ml-9">Search, Find, & Apply</h3>
+                <p className="mt-4 ml-9 text-center lg:text-left sm:text-sm md:text-base lg:text-lg xl:text-xl" style={{ borderRadius: '10px', paddingRight: '200px' }}>
+                  {/* Added text size classes for different screen sizes */}
+                  lorem ipsum dolor sit amet, consectetur adipiscing elit, Sed quis lacus non orci euismod vestibulum vitae ut ex, Quisque ut arcu at lectus tristique auctor sit amet at turpis.
+                </p>
+              </div>
+              <div className="bg-white flex items-center mt-10 mb-16 ml-20" style={{ borderRadius: '15px' }}>
+                <button className="bg-gray-200 flex text-black p-2 ml-4 mr-4 pr-16" style={{ borderRadius: '10px' }}>
+                  <img src="/searchicon.png" alt="Location Icon" className="mr-2 mt-1 h-4 w-4" />
+                  Job title or keyword
+                </button>
+                <button className="flex bg-gray-200 text-black p-2 m-2 pr-16 mr-4" style={{ borderRadius: '10px' }}>
+                  <img src="/locationicon.jpg" alt="Location Icon" className="mr-2 mt-1 h-4 w-4" />
+                  Location
+                </button>
+                <button className="bg-green-400 text-white p-2 m-2 pl-10 pr-10 ml-4 " style={{ borderRadius: '10px' }}>Search</button>
+              </div>
+            </div>
           </div>
-          <div className="bg-white flex items-center mt-10 mb-16 ml-16" style={{ borderRadius: '15px' }}>
-            <button className="bg-gray-200 flex text-black p-2 m-2 pr-16" style={{ borderRadius: '10px' }}>
-            <img src="/searchicon.png" alt="Location Icon" className="mr-2 mt-1 h-4 w-4"  />
-            Job title or keyword
-            </button>
-            <button className=" flex bg-gray-100 text-black p-2 m-2 pr-16" style={{ borderRadius: '10px' }}>
-              <img src="/locationicon.jpg" alt="Location Icon" className="mr-2 mt-1 h-4 w-4"  />
-              Location
-            </button>
-            <button className="bg-green-400 text-white p-2 m-2 pl-10 pr-10" style={{ borderRadius: '10px' }}>Search</button>
+          {/* Green background */}
+          <div className="bg-green-300 w-full lg:w-1/2" style={{ borderBottomLeftRadius: '55px' }}>
+            <div className="p-10">
+              <img src="/image1.jpg" alt="Image 1" className="w-full mb-4" />
+              <img src="/image2.jpg" alt="Image 2" className="w-full mb-4" />
+              <img src="/image3.jpg" alt="Image 3" className="w-full" />
+            </div>
           </div>
-        </div>
-       
-        <div className="bg-green-300 w-1/2 p-10">
-          <img src="/image1.jpg" alt="Image 1" className="w-full mb-4" />
-          <img src="/image2.jpg" alt="Image 2" className="w-full mb-4" />
-          <img src="/image3.jpg" alt="Image 3" className="w-full" />
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,67 +10,68 @@ const Navbar = () => {
 
   return (
     <nav className="relative">
-      <div className="container mx-auto max-w-[1350px]">
-        <div className="flex justify-center">
-          {/* Gray background covering full width on small screens and left half on large screens */}
-          <div className="bg-gray-200 py-5 px-10 w-full lg:w-3/4 flex items-center">
-            <div className="flex items-center">
-              <span className="text-black font-bold text-3xl ml-20">Job</span>
-              <span className="text-green-400 font-bold text-3xl ">NQW</span>
-            </div>
-            <ul className="lg:flex hidden ml-16">
-              <li className="navItem ml-2">
-                <Link href="/" className="navLink text-black p-2 font-bold ml-2">Home</Link>
-              </li>
-              <li className="navItem mx-4">
-                <Link href="/about" className="navLink text-black p-2 font-bold">Job</Link>
-              </li>
-              <li className="navItem mx-4">
-                <Link href="/about" className="navLink text-black p-2 font-bold">AboutUs</Link>
-              </li>
-              <li className="navItem mx-4">
-                <Link href="/contact" className="navLink text-black p-2 font-bold">Contact</Link>
-              </li>
-            </ul>
-            <button onClick={toggleMenu} className="lg:hidden focus:outline-none">
-              <svg className={`h-6 w-6 fill-current ${showMenu ? 'text-white' : 'text-white'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                {showMenu ? (
-                  <path d="M19 13H5v-2h14v2zm0-5H5V6h14v2zm0 10H5v-2h14v2z" />
-                ) : (
-                  <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
-                )}
-              </svg>
-            </button>
-          </div>
-          {/* Green background covering right half of the screen */}
-          <div className="hidden lg:block bg-green-300 py-5 px-10 w-1/2 flex items-center">
-            <div className="flex items-center justify-end">
-              <div className="mr-10 text-white">Sign In</div>
-              <button className="flex bg-white text-black p-2 font-bold mr-20" style={{borderRadius: '10px'}}>
-                <img src="/arrowicon.jpg" alt="Location Icon" className="ml-3 mr-2 mt-1 h-3 w-3"  />
-                Create Account
+      <div className="bg-gray-200 py-0 lg:bg-none">
+        <div className=" nav-content container mx-auto">
+          <div className="flex">
+            <div className=" py-5  px-10 w-full lg:w-3/4 flex items-center">
+              <div className="flex items-center">
+                <span className="text-black font-bold text-3xl " style={{ marginLeft :'55px' }}>Job</span>
+                <span className="text-green-400 font-bold text-3xl">NQW</span>
+              </div>
+              <ul className="lg:flex hidden ml-16">
+                <li className="navItem ml-2">
+                  <Link href="/" className="navLink text-black p-2 font-bold ml-2">Home</Link>
+                </li>
+                <li className="navItem mx-4">
+                  <Link href="/about" className="navLink text-black p-2 font-bold">Job</Link>
+                </li>
+                <li className="navItem mx-4">
+                  <Link href="/about" className="navLink text-black p-2 font-bold">AboutUs</Link>
+                </li>
+                <li className="navItem mx-4">
+                  <Link href="/contact" className="navLink text-black p-2 font-bold">Contact</Link>
+                </li>
+              </ul>
+              <button onClick={toggleMenu} className="lg:hidden  focus:outline-none" style={{ marginLeft: '200px' }}>
+                <svg className={`h-6 w-6 fill-current ${showMenu ? 'text-white' : 'text-white'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  {showMenu ? (
+                    <path d="M19 13H5v-2h14v2zm0-5H5V6h14v2zm0 10H5v-2h14v2z" />
+                  ) : (
+                    <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
+                  )}
+                </svg>
               </button>
+            </div>
+            {/* Right section with green background */}
+            <div className="hidden lg:block bg-green-300 py-5 px-10 w-full lg:w-1/2 ml-2 flex items-center justify-end">
+              <div className="flex items-center justify-end">
+                <div className="mr-10 text-white">Sign In</div>
+                <button className="flex bg-white text-black p-2 font-bold mr-20" style={{ borderRadius: '10px' }}>
+                  <img src="/arrowicon.jpg" alt="Location Icon" className="ml-3 mr-2 mt-1 h-3 w-3" />
+                  Create Account
+                </button>
+              </div>
             </div>
           </div>
         </div>
+        
+        {showMenu && (
+          <ul className="lg:hidden absolute top-full left-0 bg-gray-200 w-full rounded z-20">
+            <li className="navItem mx-4 ">
+              <Link href="/" className="navLink text-black p-2 font-bold ml-4">Home</Link>
+            </li>
+            <li className="navItem mx-4">
+              <Link href="/about" className="navLink text-black p-2 font-bold ml-4">Job</Link>
+            </li>
+            <li className="navItem mx-4">
+              <Link href="/about" className="navLink text-black p-2 font-bold ml-4">AboutUs</Link>
+            </li>
+            <li className="navItem mx-4">
+              <Link href="/contact" className="navLink text-black p-2 font-bold ml-4">Contact</Link>
+            </li>
+          </ul>
+        )}
       </div>
-      {/* Gray background covering full width on small screens */}
-      {showMenu && (
-        <ul className="lg:hidden absolute top-full left-0 bg-gray-200 w-full py-2 rounded z-20">
-          <li className="navItem mx-4 ">
-            <Link href="/" className="navLink text-black p-2 font-bold ml-4">Home</Link>
-          </li>
-          <li className="navItem mx-4">
-            <Link href="/about" className="navLink text-black p-2 font-bold ml-4">Job</Link>
-          </li>
-          <li className="navItem mx-4">
-            <Link href="/about" className="navLink text-black p-2 font-bold ml-4">AboutUs</Link>
-          </li>
-          <li className="navItem mx-4">
-            <Link href="/contact" className="navLink text-black p-2 font-bold ml-4">Contact</Link>
-          </li>
-        </ul>
-      )}
     </nav>
   );
 };
